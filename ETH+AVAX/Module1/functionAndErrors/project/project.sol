b9503eeb647c330c44f4256c2a983e97f74f76ea
Harmandeep Harmandeep <harmandeep02022@outlook.com>
2024-07-28 08:21:22 +0000

    project.sol file Code Updated

--- ETH+AVAX/Module1/functionAndErrors/project/project.sol
+++ ETH+AVAX/Module1/functionAndErrors/project/project.sol
@@ -1,49 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
-contract RestaurantLoyalty {
-    string public name = "Restaurant Loyalty Program";
-    string public symbol = "HRT";
-    uint256 public totalSupply;
-    address public owner;
+contract GCD {
 
-    mapping(address => uint256) public balanceOf;
+    function gcd(uint a, uint b) public pure returns (uint) {
+        require(a > 0, "Input a must be greater than 0");
+        require(b >= 0, "Input b must be non-negative");
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event RewardRedeemed(address indexed customer, string reward, uint256 cost);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "You are not the owner");
-        _;
-    }
-
-    constructor(uint256 _initialSupply) {
-        owner = msg.sender;
-        totalSupply = _initialSupply;
-        balanceOf[owner] = totalSupply;
-    }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function issueTokens(address _to, uint256 _value) public onlyOwner returns (bool success) {
-        require(totalSupply + _value <= type(uint256).max, "Total supply exceeded");
-        totalSupply += _value;
-        balanceOf[_to] += _value;
-        emit Transfer(address(0), _to, _value);
-        return true;
-    }
-
-    function redeemReward(address _customer, string memory _reward, uint256 _cost) public onlyOwner returns (bool success) {
-        require(balanceOf[_customer] >= _cost, "Insufficient tokens");
-        balanceOf[_customer] -= _cost;
-        balanceOf[owner] += _cost;
-        emit RewardRedeemed(_customer, _reward, _cost);
-        return true;
+        if (b == 0) {
+            return a;
+        } else {
+            uint result = gcd(b, a % b);
+            assert(result > 0);
+            return result;
+        }
     }
 }
